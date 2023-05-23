f8bef1606b14148f2e7bb5b9c9c6104961ba6386
Asus <Bichk_di78@student.itstep.org>
2023-05-23 18:56:11 +0300

    запис в .log

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import logging #стандартний
-logging.basicConfig(level = logging.DEBUG , format = '%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level = logging.DEBUG ,
+                    filename = "logs.log" ,
+                    filemode = 'w' ,
+                    format ='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug("debug")
 logging.info("info")
 logging.error("error")
