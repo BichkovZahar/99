66bf63c7fa191b50ca8fd3c628435195024f0715
Asus <Bichk_di78@student.itstep.org>
2023-05-23 19:14:18 +0300

    використання info()

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ try:
     print(10 / 0)
 except Exception:
     logging.exception("GG")
+#факторіал числа
+def factorial(n):
+    logging.info(f"Розпочато обчислення факторіалу числа{n}")
+    rezult = 1
+    for i in range(1 , n + 1):
+        rezult *= i  #1*2*3...
+    logging.info(f"Обчислення факторіалу Резутьтат {rezult}")
+    return rezult
+logging.basicConfig(level= logging.INFO)
+factorial(5)
