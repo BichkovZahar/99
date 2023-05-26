e9184190131e4b9bdd9b17de63864c7a92786861
Asus <Bichk_di78@student.itstep.org>
2023-05-26 10:21:54 +0300

    BeautifulSoup

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text , features="html.parser")
     soup_list = soup.find_all("a" , {'href' : "/currencies/bitcoin/markets/"})
     res = soup_list[0].find('span')
-    print("Курс біткоіна:" , res.text)
+    print("Курс біткоіна:", res.text)
 
 
