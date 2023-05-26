81e68b6d22e5d7a041be48853ff89338fea245f4
Asus <Bichk_di78@student.itstep.org>
2023-05-26 08:59:22 +0300

    знайомство з реквест

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import urllib.request
 opener = urllib.request.build_opener()
 response = opener.open("https://httpbin.org/get")
 print(response.read())
+import requests
+response = requests.get('https://coinmarketcap.com/')
+print(response.text)
