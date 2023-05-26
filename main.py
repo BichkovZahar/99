b8a86e823a45c7ef2447a5b28db5a6514279822d
Asus <Bichk_di78@student.itstep.org>
2023-05-26 10:19:45 +0300

    BeautifulSoup

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ response_parse = response.text.split('<span>')
 for elen in response_parse:
     if elen.startswith("$"):
         print(elen)
+from bs4 import BeautifulSoup
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text , features="html.parser")
+    soup_list = soup.find_all("a" , {'href' : "/currencies/bitcoin/markets/"})
+    res = soup_list[0].find('span')
+    print("Курс біткоіна:" , res.text)
+
+
