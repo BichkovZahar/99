4d1cc0de5f44081c7d15497868d3c80f58ae17ae
Asus <Bichk_di78@student.itstep.org>
2023-05-26 09:14:52 +0300

    знайомство з реквест

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ print(response.read())
 import requests
 response = requests.get('https://coinmarketcap.com/')
 print(response.text)
+response_parse = response.text.split('<span>')
+for elen in response_parse:
+    if elen.startswith("$"):
+        print(elen)
